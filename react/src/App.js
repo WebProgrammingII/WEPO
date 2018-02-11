9ac192f1d6566890b06f165465c02f382406fa52
Arnar Leifsson <arnarl@ru.is>
2018-02-11 15:38:01 +0000

    Extending the project description

--- react/src/App.js
+++ react/src/App.js
@@ -3,12 +3,17 @@ import ReactDOM from 'react-dom';
 import MainContent from './components/MainContent/MainContent';
 import '../styles/site.less';
 
-const App = () => {
-    return (
-        <div>
-            <MainContent />
-        </div>
-    );
-};
+class App extends React.Component {
+    // define getChildContext
+    render() {
+        return (
+            <div>
+                <MainContent />
+            </div>
+        );
+    };
+}
+
+// Define childContextTypes and use contextTypes for navigation bar
 
 ReactDOM.render(<App />, document.getElementById('app'));
