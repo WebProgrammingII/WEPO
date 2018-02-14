52ef231d9bc41223d4db4cb24087b2a0757343fc
Arnar Leifsson <drleifz@gmail.com>
2018-02-14 22:33:28 +0000

    Adding .eslintrc as well

--- dev_env/webpack.config.js
+++ dev_env/webpack.config.js
@@ -2,5 +2,7 @@
     Should contain a basic webpack config which uses entry.js as an entry point and bundles all together
     using Babel and bundles all into /dist, static assets such as *.less and *.jpg should be exported as well
 
-    At the end use uglify to minify bundle webpack.optimize.UglifyJsPlugin
+    At the end use uglify to minify bundle with webpack.optimize.UglifyJsPlugin()
+
+    Plug eslint-loader in to the mix and see some coding errors!
 **/
