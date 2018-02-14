9f1754c713a876c197713c8e133b204a93f749f2
Arnar Leifsson <drleifz@gmail.com>
2018-02-14 19:12:22 +0000

    Adding demonstration methods for friday class

---
+++ es6_methods/default_exports.js
@@ -0,0 +1,1 @@
+// Export default export for service here
---
+++ es6_methods/es6_methods_examples.js
@@ -0,0 +1,31 @@
+/*
+    ES6 methods examples
+**/
+
+// 1. Define a function which shows how const and let are block scoped, vs. var is function scoped
+
+// 2. Import two methods from named exports sum and divide
+
+// 3. Import a service as a default export
+
+// 4. Use Object.assign() with two params
+
+// 5. Use Object.assign() with three params
+
+// 6. Use Object.assign() with four params
+
+// 7. Show how template literals can be used by creating the string Random number: {someRandomNumber}
+
+// 8. Use the spread operator with two arrays
+
+// 9. Use the spread operator in to a function which console logs its arguments
+
+// 10. Use the spread operator to spread an array to console log
+
+// 11. Define an object which makes use of enhanced object literals
+
+// 12. Define a function called add element to array and make use of default params
+
+// 13. Destructure a user object
+
+// 14. Destructure a list of three string
